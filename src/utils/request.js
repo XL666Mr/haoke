915ce57f6c0e757e59332f187a3b805748748bc4
XL_Mr <643158351@qq.com>
2022-07-17 17:29:13 +0800

    favorate--50%

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,14 +1,13 @@
 import axios from 'axios'
+import store from '@/store'
 const request = axios.create({
   baseURL: 'http://liufusong.top:8080'
 })
 request.interceptors.request.use((config) => {
-  config.headers.authorization =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJRCI6MzE2LCJpYXQiOjE2NTc4ODYwOTYsImV4cCI6MTY1NzkwMDQ5Nn0.nJ8LHYwNgbGp-ylvPHvvOTQbxl8JOADnvDJyYg8jRBU'
+  config.headers.authorization = store.state.token.token
   return config
 })
 request.interceptors.response.use((res) => {
-  console.log(res)
   return res
 })
 export default request
--- src/views/Favorate/index.vue
+++ src/views/Favorate/index.vue
@@ -9,14 +9,18 @@
     </div>
     <div>
       <van-list>
-        <van-cell
-          v-for="(item, index) in list"
-          :key="index"
-          :title="item.title"
-        >
-          <template>
-            <img :src="url + item.houseImg" />
-          </template>
+        <van-cell v-for="(item, index) in list" :key="index" class="Fa_grid">
+          <div class="Fa_template">
+            <div class="Fa_img">
+              <img :src="url+item.houseImg" />
+            </div>
+            <div class="Fa_right">
+              <div><h3>{{item.title}}</h3></div>
+              <div class="Fa_text">{{item.desc}}</div>
+              <div class="Fa_site"><span v-for="(obj,i) in item.tags" :key="i">{{obj}}</span></div>
+              <div class="Fa_price"><span>{{item.price}}</span>元/月</div>
+            </div>
+          </div>
         </van-cell>
       </van-list>
     </div>
@@ -26,22 +30,22 @@
 <script>
 import { getList } from '@/api/user'
 export default {
-  data () {
+  data() {
     return {
       list: [],
       url: 'http://liufusong.top:8080'
     }
   },
-  created () {
+  created() {
     this.getData()
   },
   methods: {
-    async getData () {
+    async getData() {
       const { data: res } = await getList()
       console.log(res.body)
       this.list = res.body
     },
-    onClickLeft () {
+    onClickLeft() {
       this.$router.back()
     }
   }
@@ -56,6 +60,58 @@ export default {
   }
   :deep .van-icon-arrow-left:before {
     color: #fff;
+  }
+}
+.Fa_grid {
+  height: 240px;
+  padding-top: 18px;
+  :deep .van-cell__value {
+    display: flex;
+    .Fa_template {
+      display: flex;
+      align-items: center;
+      // justify-content: center;
+      .Fa_img {
+        width: 106 * 2px;
+        height: 80 * 2px;
+        display: flex;
+        align-items: center;
+        img {
+          display: block;
+          width: 100%;
+          height: 100%;
+        }
+      }
+    }
+    .Fa_right {
+      padding-left: 20px;
+      h3 {
+        margin: 0;
+      }
+      .Fa_text {
+        font-size: 12px;
+        color: #afb2b3;
+      }
+      .Fa_site {
+        color: #39becd !important;
+        background-color: #e1f5f8;
+        width: 46 * 2px;
+        height: 40px;
+        line-height: 40px;
+        font-size: 12px;
+        text-align: center;
+        margin-right: 10px;
+      }
+      .Fa_price {
+        font-size: 12px;
+        color: #fa5741;
+        span {
+          font-size: 32px;
+          font-weight: bolder;
+          margin-right: 10px;
+        }
+      }
+    }
   }
 }
 </style>
